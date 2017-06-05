51619aa0c932494615f6e0fbd6f8a410b0f3432e
Ruiyi Zheng <scott.ruiyi.zheng@utexas.edu>
2017-06-05 14:10:47 -0500

    passed all tests for leap year exercise

---
+++ javascript/leap/leap.js
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the "Leap" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Year = function(input) {
+  this.year = input;
+}
+
+Year.prototype.isLeap = function() {
+  if(this.year % 4 == 0){
+    if(this.year % 100 == 0){
+      if (this.year % 400 == 0){
+        return true;
+      }
+      return false;
+    }
+    return true;
+  }
+  return false;
+}
+
+module.exports = Year;
